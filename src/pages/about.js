959b20f5664cb8eef47c38873261a14a43bfc88a
Andrew Smith <ajs3993@gmail.com>
2020-05-18 19:23:07 -0500

    about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About me</h1>
+      <p>blah blah blah</p>
+    </div>
+  )
+}
+
+export default AboutPage
